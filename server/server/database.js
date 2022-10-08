9c5c470c752b32f5cb29f8d0a9e87e7e1ca21566
DucllOwO <nguyenduc147862@gmail.com>
2022-10-08 10:57:52 +0700

    Revert "add route"
    This reverts commit 7b5f68b21745e0a2c5ff5559feb9ac83ff999912.

--- server/server/database.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
