16ed03b5269d52f98130e3f82af5fa322c4a517d
DucllOwO <nguyenduc147862@gmail.com>
2022-11-04 15:54:17 +0700

    complete user route

---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,64 @@
+// delete
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController.js");
+const authorizeAccessToken = require("../middlewares/authorizeAccessToken");
+const { hasPermission } = require("../middlewares/roleAccessControl");
+const { actionAC, resourceAC } = require("../utils/constants");
+const { tryCatch } = require("../middlewares/errorHandler");
+const router = require("express").Router();
+
+router.get(
+  "/",
+  authorizeAccessToken,
+  hasPermission(actionAC.GET, resourceAC.USER),
+  tryCatch(getAllUsers)
+);
+
+router.get(
+  "/:id",
+  authorizeAccessToken,
+  hasPermission(actionAC.GET, resourceAC.USER),
+  tryCatch(getUser)
+);
+
+// router.get(
+//   "/me",
+//   authorizeAccessToken,
+//   hasPermission(actionAC.GET, resourceAC.USER),
+//   tryCatch(getMyAccount)
+// );
+
+router.post(
+  "/",
+  authorizeAccessToken,
+  hasPermission(actionAC.CREATE, resourceAC.USER),
+  tryCatch(createUser)
+);
+
+router.put(
+  "/:id",
+  authorizeAccessToken,
+  hasPermission(actionAC.UPDATE, resourceAC.USER),
+  tryCatch(updateUser)
+);
+
+router.delete(
+  "/:id",
+  authorizeAccessToken,
+  hasPermission(actionAC.DELETE, resourceAC.USER),
+  tryCatch(deleteUser)
+);
+
+// router.post(
+//   "/me/logout",
+//   authorizeAccessToken,
+//   hasPermission(actionAC.CREATE, resourceAC.USER),
+//   tryCatch(logout)
+// );
+
+module.exports = router;
