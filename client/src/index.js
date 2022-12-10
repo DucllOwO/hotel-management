611887ff22a1fe7f59083d2c133fee7e06b1113f
DucllOwO <nguyenduc147862@gmail.com>
2022-12-10 22:17:06 +0700

    test custom theme

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import { ConfigProvider, theme } from "antd";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -5,7 +6,19 @@ import { AppProvider } from "./context/AppContext";
 
 ReactDOM.render(
   <AppProvider>
-    <App />
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          colorPrimary: "#A0D911",
+          colorPrimaryBg: "#455a19",
+          fontSize: 16,
+          colorBgBase: "#2b342b",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </AppProvider>,
   document.getElementById("root")
 );
