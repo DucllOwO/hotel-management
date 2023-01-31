3c60488b8d025a2bc182719ef748fb187305d68e
Kim Dien <20520442@gm.uit.edu.vn>
2023-01-31 15:44:04 +0700

    responsive admin

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./admin.css";
 import Navbar from "../../components/Admin/Navbar/Navbar";
 import Topbar from "../../components/Topbar/Topbar";
@@ -7,7 +7,10 @@ import LocalStorage from "../../Utils/localStorage";
 import { userRequest } from "../../api/api";
 import ErrorBoundary from "../../components/Error/ErrorBoundary/ErrorBoundary";
 
+
+
 const Admin = () => {
+  const [collapsed, setCollapsed] = useState(true);
   useEffect(() => {
     document.title = "Admin | Parallel Shine";
   });
@@ -19,16 +22,21 @@ const Admin = () => {
 
   return (
     <ErrorBoundary>
-      <div className="container">
+      <div className="mainContainer">
         {!user && <Navigate to="/" replace={true} />}
-        <Navbar className="navbar"></Navbar>
+        {/* <Navbar className="navbar"></Navbar> */}
+        {collapsed ? <Navbar className="navbar"></Navbar> : null}
         <div className="homeContainer">
           <Topbar
+            collapsed={collapsed}
+            setCollapsed={setCollapsed}
             name="Huỳnh Thế Vĩ"
             img="https://12ax7web.s3.amazonaws.com/accounts/1/products/1986199880924/Boba-Stitch_800x800_SEPS-1000x1000.jpg"
             position="Quản lý"
           ></Topbar>
-          <Outlet />
+          <div className={collapsed ? "blur" : "normal"}>
+            <Outlet />
+          </div>
         </div>
       </div>
     </ErrorBoundary>
