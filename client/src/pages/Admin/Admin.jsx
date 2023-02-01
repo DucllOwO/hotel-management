aa1fd847f9b01949dd1fb412179a8fc041f8ae31
Kim Dien <20520442@gm.uit.edu.vn>
2023-02-01 11:23:52 +0700

    blur background

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -10,7 +10,7 @@ import ErrorBoundary from "../../components/Error/ErrorBoundary/ErrorBoundary";
 
 
 const Admin = () => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [collapsed, setCollapsed] = useState(false);
   useEffect(() => {
     document.title = "Admin | Parallel Shine";
   });
@@ -25,7 +25,7 @@ const Admin = () => {
       <div className="mainContainer">
         {!user && <Navigate to="/" replace={true} />}
         {/* <Navbar className="navbar"></Navbar> */}
-        {collapsed ? <Navbar className="navbar"></Navbar> : null}
+        {!collapsed ? <Navbar className="navbar"></Navbar> : null}
         <div className="homeContainer">
           <Topbar
             collapsed={collapsed}
@@ -34,7 +34,7 @@ const Admin = () => {
             img="https://12ax7web.s3.amazonaws.com/accounts/1/products/1986199880924/Boba-Stitch_800x800_SEPS-1000x1000.jpg"
             position="Quản lý"
           ></Topbar>
-          <div className={collapsed ? "blur" : "normal"}>
+          <div className={!collapsed ? "blur" : "normal"}>
             <Outlet />
           </div>
         </div>
