d202fad1bc4aa02d7ce1d7439c231c03146003b7
Kim Dien <20520442@gm.uit.edu.vn>
2023-02-03 11:13:03 +0700

    receipt done

--- client/src/pages/Customer/Home/Receipt/Receipt.jsx
+++ client/src/pages/Customer/Home/Receipt/Receipt.jsx
@@ -2,20 +2,96 @@ import React from "react";
 import TopBar from "../../../../components/Customer/TopBar/TopBar";
 import DetailRoomTable from "../../../../components/Form/DetailForm/Tables/DetailRoomTable";
 import styled from "styled-components";
-import { Button, Col, Form, Row } from "antd";
+import { Button, Col, Form, Row, Table } from "antd";
 
 const DetailContainer = styled.div`
   justify-content: space-between;
-  padding: 0 36px;
+  padding: 0 150px;
   
 `;
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  padding: 0 36px;
+  padding: 10px 150px;
   
 `;
 const CustomerReceipt = () => {
+    const columns = [
+        {
+          key: "1",
+          title: "Tên Phòng",
+          dataIndex: "room_name",
+          align: "center",
+          render: (text, record) => {
+            return String(record.roomInfo.room_name);
+          },
+        },
+        {
+          key: "2",
+          title: "Loại phòng",
+          dataIndex: "room_type",
+          align: "center",
+          render: (text, record) => {
+            return String(record.roomInfo.room_type_id.name);
+          },
+        },
+        {
+          key: "3",
+          title: "Giờ đầu tiên (đ)",
+          dataIndex: "price",
+          align: "center",
+          render: (value, record) => {
+            console.log(record);
+            return String(
+              record.roomInfo.price.first_hour_price
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            );
+          },
+        },
+        {
+          key: "4",
+          title: "Một giờ (đ)",
+          dataIndex: "price",
+          align: "center",
+          render: (value, record) => {
+            console.log(record);
+            return String(
+              record.roomInfo.price.hour_price
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            );
+          },
+        },
+        {
+          key: "5",
+          title: "Một ngày (đ)",
+          dataIndex: "price",
+          align: "center",
+          render: (value, record) => {
+            console.log(record);
+            return String(
+              record.roomInfo.price.one_day_price
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            );
+          },
+        },
+        {
+          key: "6",
+          title: "Qua đêm (đ)",
+          dataIndex: "price",
+          align: "center",
+          render: (value, record) => {
+            console.log(record);
+            return String(
+              record.roomInfo.price.overnight_price
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            );
+          },
+        },
+      ];
     const dataSource = [{
         roomInfo:{
             room_name: "A001",
@@ -58,7 +134,17 @@ const CustomerReceipt = () => {
                 </div>
             </div>
             <div>
-                <DetailRoomTable dataSource={dataSource}></DetailRoomTable>
+                <Table
+                    pagination={false}
+                    columns={columns}
+                    dataSource={dataSource}
+                    style={{ width: "100%" }}
+                    scroll={{ y: "100%" }}
+                    rowKey={(row) => {
+                    return row.room_id;
+                    }}
+                    bordered={true}
+                ></Table>
             </div>
             <hr />
             <Row justify={"end"}>
@@ -68,14 +154,14 @@ const CustomerReceipt = () => {
                 </Col>
                 <Col span={7}>
                 <div className="itemListContainer">
-                    <div className="itemList">Tổng tiền phòng: </div>
-                    <div className="itemList">Tổng tiền dịch vụ</div>
-                    <div className="itemList">Phụ thu</div>
-                    <div className="totalTitle">Tổng cộng</div>
+                    {/* <div className="itemList">Tổng tiền phòng: </div>
+                    <div className="itemList">Tổng tiền dịch vụ:</div>
+                    <div className="itemList">Phụ thu:</div> */}
+                    <div className="totalTitle">Tổng cộng:</div>
                 </div>
                 </Col>
                 <Col span={5}>
-                <div className="priceList">
+                {/* <div className="priceList">
                     500,000
                 </div>
                 <div className="priceList">
@@ -83,13 +169,16 @@ const CustomerReceipt = () => {
                 </div>
                 <div className="priceList">
                     50,000
-                </div>
+                </div> */}
                 <div className="totalText">
-                    650,000
+                    500,000
                 </div>
                 </Col>
             </Row>
             </DetailContainer>
+            <ButtonContainer>
+                <Button type="primary">Xong</Button>
+            </ButtonContainer>
         </div>
     )
 }
